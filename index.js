16379d5870f67ceb8e716c403c7d158d26cdbe3c
Monica <monijerobon@gmail.com>
2023-03-15 12:47:14 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance) {
+
+  if (distance<=400)
+  {
+
+    return `This one is on me!`;
+  }else if(distance<=2499 )
+  {
+    return `I will gladly take your thirty bucks.`;
+  }else
+  {
+    return `No can do.`;
+  }
+}
+function ternaryCheckCity(city)  {
+
+  return city === `NYC` ? "Ok, sounds good." : "No go.";
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+switch(tip){
+  case `generous` :
+    return `Thank you so much.`;
+    break;
+    case `not as generous` :
+      return `Thank you.`;
+      break;
+      case `thanks for everything`:
+        return `Bye.`;
+      
+      
+}
 }
